e43b2cb53eebc8f5ffa157e90bb5738171185c99
chasingSublimity <blakemsager@gmail.com>
2017-01-09 16:43:45 -0600

    add PUT method

--- models.js
+++ models.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 const uuid = require('uuid');
 
 // this module provides volatile storage, using a `BlogPost`
@@ -36,7 +38,7 @@ const BlogPosts = {
     // return posts sorted (descending) by
     // publish date
     return this.posts.sort(function(a, b) {
-      return b.publishDate - a.publishDate
+      return b.publishDate - a.publishDate;
     });
   },
   delete: function(id) {
@@ -52,7 +54,7 @@ const BlogPosts = {
       post => post.id === updatedPost.id);
     if (postIndex === -1) {
       throw StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`)
+        `Can't update item \`${id}\` because doesn't exist.`);
     }
     this.posts[postIndex] = Object.assign(
       this.posts[postIndex], updatedPost);
--- server.js
+++ server.js
@@ -12,17 +12,34 @@ const app = express();
 
 app.use(morgan('common'));
 
-BlogPosts.create('title1', 'text1', 'blake sager', null);
-BlogPosts.create('title2', 'text2', 'blake sager', null);
-BlogPosts.create('title3', 'text3', 'blake sager', null);
-BlogPosts.create('title4', 'text4', 'blake sager', null);
+BlogPosts.create('JavaScript and You', 'text1', 'blake sager', null);
+BlogPosts.create('Assembly for Dummies', 'text2', 'blake sager', null);
+BlogPosts.create('Who moved my trackpad?', 'text3', 'blake sager', null);
+BlogPosts.create('Logs. So many logs.', 'text4', 'blake sager', null);
 
 app.get('/blog-posts', (req, res) => {
 	res.json(BlogPosts.get()); 
 });
 
-app.post('/blog-posts');
+// what is jsonParser doing and how?
+app.post('/blog-posts', jsonParser, (req, res) => {
+	const requiredFields = ['title', 'content', 'author', 'publishDate'];
+	// ask about let and for loops w/ aysnc code
+	for (let i=0; i < requiredFields.length; i++) {
+		if(!(requiredFields[i] in req.body)) {
+			const msg = 'Missing ${field} in request body';
+			console.log(msg);
+			return res.satus(400).send(msg);
+		}
+	}
+	const blogPost = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+	res.status(201).json(blogPost);
+});
 
-app.delete('/blog-posts/:id');
+// app.delete('/blog-posts/:id');
 
-app.put('/blog-posts/:id');
+// app.put('/blog-posts/:id');
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
